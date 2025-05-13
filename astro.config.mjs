6676a110ffa68b1ec7ca6c6e944151e312ce5348
Devon <devon.dana@gmail.com>
2025-05-13 13:55:37 -0400

    Generate a new astro project

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    // server: {
+    //   https: true,
+    // },
+  },
+});
